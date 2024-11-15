f6c0046f04d15d38ae406acccff18a5e876e43f6
cmarinbustam_meli <camilo.marinbustamante@mercadolibre.com.co>
2024-11-15 13:34:41 -0500

    fix: fix typo on buyer entity

--- internal/entities/buyers.go
+++ internal/entities/buyers.go
@@ -1,11 +1,12 @@
 package entities
 
+// Buyer represents a buyer who buy on the shop
 type Buyer struct {
 	ID int
-	BuyeerAttributes
+	BuyerAttributes
 }
 
-type BuyeerAttributes struct {
+type BuyerAttributes struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Address   string `json:"address"`
